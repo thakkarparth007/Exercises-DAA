c805dcbc29f540a8a64873549b1ba49d3b3af6b2
Parth Thakkar <thakkarparth007@gmail.com>
2015-05-25 01:55:05 +0530

    Exercise 2.5 Finished

---
+++ ch02/2.5.10.py
@@ -0,0 +1,52 @@
+from timeit import Timer
+from math import floor
+
+MOD = 100000	# to calculate last 5 digits, compute everything mod this.
+
+def fib_rec(n):
+	if n == 0:
+		return 0
+	if n == 1:
+		return 1
+	return ( fib_rec(n-1) + fib_rec(n-2) ) % MOD
+
+def fib_iter(n):
+	a,b = 0,1
+	c = 0
+	if n == 0:	return 0
+	if n == 1:	return 1
+
+	i = 2
+	while i <= n:
+		c = (a+b) % MOD
+		a = b
+		b = c
+		i = i+1
+	return c
+
+print """This program will compute the maximum values of n, such that 
+the nth Fibonacci number is computed under 1 minute, using 
+both - recursive and iterative algorithms."""
+
+# n = 0
+# t = 0
+# while t < 60:
+# 	t = Timer("fib_rec(n)", "from __main__ import fib_rec, n").timeit(1)
+# 	print "Time for n=%d is %fs\n" % (n, t)
+# 	n = n+1
+
+# print "*******************************************************\n"
+# print "Max n using recursive algorithm: %d\n" % (n-1,)
+# print "*******************************************************\n"
+
+n = 201326592
+t = 0
+while t < 60:
+	print "Now timing n=%d\n" % (n,)
+	t = Timer("fib_iter(n)", "from __main__ import fib_iter, n").timeit(1)
+	print "Time for n=%d is %fs\n" % (n, t)
+	n = floor(1.5*n)
+
+print "*******************************************************\n"
+print "Max n using iterative algorithm: %d\n" % (n-1,)
+print "*******************************************************\n"
